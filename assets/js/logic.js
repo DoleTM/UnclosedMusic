e8db21d426e80b10abae7b72048c22347a99b187
DoleTM <adylantoremember@gmail.com>
2019-11-27 23:52:18 -0500

    Added side nav

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,3 @@
-// Menu functionality
+// Menu Functionality
+const slideMenu = document.querySelectorAll('.sidenav');
+M.Sidenav.init(slideMenu, {edge: 'right', inDuration: 500, outDuration: 500});
