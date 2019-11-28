f7cfce70092d6382d1ea9ba6cfc1cdb62ba82131
DoleTM <adylantoremember@gmail.com>
2019-11-28 01:48:03 -0500

    Parallax started, work in progress

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,6 @@
 // Menu Functionality
-const slideMenu = document.querySelectorAll('.sidenav');
-M.Sidenav.init(slideMenu, {edge: 'right', inDuration: 500, outDuration: 500});
+const sideMenu = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideMenu, {edge: 'right', inDuration: 500, outDuration: 500});
+
+const pllax = document.querySelectorAll('.parallax');
+M.Para.init(pllax, {responsiveTheshold: 0});
