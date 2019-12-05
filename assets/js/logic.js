7bc42fcc5b22c6a4afab7d362181435d03e8e675
DoleTM <adylantoremember@gmail.com>
2019-12-05 16:58:40 -0500

    releases.html started with embedded player

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,39 +1,44 @@
 // Menu Functionality
 const sideMenu = document.querySelectorAll('.sidenav');
-M.Sidenav.init(sideMenu, {edge: 'right', inDuration: 500, outDuration: 500});
+M.Sidenav.init(sideMenu, { edge: 'right', inDuration: 500, outDuration: 500 });
 
 const pllax = document.querySelectorAll('.parallax');
-M.Parallax.init(pllax, {responsiveTheshold: 0});
+M.Parallax.init(pllax, { responsiveTheshold: 0 });
 
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
+};
 
-  const images = document.querySelectorAll('.slide-in');
-  function checkSlide(e) {
-    images.forEach(image => {
-      // Slide in at half way through the image
-      const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
-      const imageBottom = image.offsetTop + image.height;
-      const isHalfShown = slideInAt > image.offsetTop;
-      const notScrolledPast = window.scrollY < imageBottom;
-      if (isHalfShown && notScrolledPast) {
-        image.classList.add('active');
-      } else {
-        image.classList.remove('active');
-      }
-    });
-  }
+const images = document.querySelectorAll('.slide-in');
+function checkSlide(e) {
+  images.forEach(image => {
+    // Slide in at half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const notScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && notScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
 
-  window.addEventListener('scroll', debounce(checkSlide));
+window.addEventListener('scroll', debounce(checkSlide));
+
+// const iframeElement = document.querySelector('iframe');
+// const iframeElementID = iframeElement.id;
+// const widget1 = SC.Widget(iframeElement);
+// const widget2 = SC.Widget(iframeElementID);
